07683eefa2611065f3593b7ca23b737d6f3d56b2
Walter C. Nogueira Neto <walternogueira.dev@gmail.com>
2022-09-28 17:34:28 -0300

    Primeiro commit

---
+++ Entities/Login.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostelManagementSystem.Entities
+{
+    internal static class Login
+    {
+        private static string _id = "ADMIN";
+        private static string _password = "ADMIN";
+
+        public static bool UserCheck(string id, string password)
+        {
+            if (id != _id && password != _password)
+            {
+                return false;
+            }
+            else if (id != _id && password == _password)
+            {
+                return false;
+            }
+            else if (id == _id && password != _password)
+            {
+                return false;
+            }
+            else
+                return true;
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+using HostelManagementSystem.Entities;
+
+namespace HostelManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n\t\tWELCOME TO PORTO HOSTEL MANAGEMET\n\n");
+
+            Console.Write("\tLogin\n\t    Id: ");
+            string id = Console.ReadLine().ToUpper();
+            Console.Write("\t    Password: ");
+            string password = Console.ReadLine().ToUpper();
+            Console.WriteLine("\n\nCheking...");
+            Thread.Sleep(3000);
+            Console.Clear();
+
+            DoLogin(id, password);
+            Thread.Sleep(2000);
+            Console.Clear();
+
+            
+            
+        }
+        static void DoLogin(string id, string password)
+        {
+
+            if (Login.UserCheck(id, password) == false)
+            {
+                while(Login.UserCheck(id, password) == false)
+                {
+
+                    Console.WriteLine("ID or PASSWORD is incorrect. Try again\n");
+                    Console.Write("Login\nId: ");
+                    id = Console.ReadLine().ToUpper();
+                    Console.Write("Password: ");
+                    password = Console.ReadLine().ToUpper();
+
+                    Console.WriteLine("Cheking...");
+
+                    Login.UserCheck(id, password);
+                    Thread.Sleep(3000);
+                    Console.Clear();
+                }
+            }
+
+            if (Login.UserCheck(id, password) == true)
+            {
+                Console.WriteLine("Allowed access");
+                Thread.Sleep(3000);
+                Console.Clear();
+            }
+
+
+
+        }
+
+        static void MainMenu()
+        {
+
+        }
+    }
+}
